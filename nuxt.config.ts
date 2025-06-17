84b532a0da7b3d7082607caa035e31f7032f52eb
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-06-17 14:21:58 +0100

    instalation Nuxt Js

---
+++ nuxt.config.ts
@@ -0,0 +1,26 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+    $production: {
+    routeRules: {
+      '/**': { isr: true }
+    }
+  },
+  $development: {
+    //
+  },
+  $env: {
+    staging: {
+      // 
+    }
+  },
+    runtimeConfig: {
+    // The private keys which are only available server-side
+    apiSecret: '123',
+    // Keys within public are also exposed client-side
+    public: {
+      apiBase: '/api'
+    }
+  }
+})
